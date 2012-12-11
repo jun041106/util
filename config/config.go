cce945decf17266a5227a4f212bae26db187e1c4
khazanovsky <khazanovsky@yahoo.com>
2012-12-11 15:47:38 -0800

    Initial checkin

---
+++ config/config.go
@@ -0,0 +1,22 @@
+// Copyright 2012 Apcera Inc. All rights reserved.
+
+package config
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+
+func parseConfig(v interface{}, defaultFilename string) error {
+
+	if v == nil {
+		panic("config structure is nil")
+	}
+	
+	
+	
+
+}
+
