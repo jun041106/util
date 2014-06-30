fc105cf4597bed93fa46540a2af2bf0945d6b2e1
Oleg Shaldybin <oleg@apcera.com>
2014-06-30 16:06:49 -0700

    Docker registry client
    Will be used by both APC and Docker stager in order to coordinate
    running Docker images.

---
+++ docker/registry.go
@@ -0,0 +1,247 @@
+// Copyright 2014 Apcera Inc. All rights reserved.
+
+package docker
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
+)
+
+const (
+	dockerDefaultRepoPrefix = "library"
+)
+
+var (
+	dockerIndexURL = "https://index.docker.io" // Tests can change it to point to a mock registry.
+)
+
+// Image is a Docker image info (constructed from Docker API response).
+type Image struct {
+	Name string
+
+	tags      map[string]string // Tags available for the image.
+	endpoints []string          // Docker registry endpoints.
+	token     string            // Docker auth token.
+
+	// scheme is an original index URL scheme (will be used to talk to endpoints returned by API).
+	scheme string
+	client *http.Client
+}
+
+// GetImage fetches Docker repository information from Docker index.
+func GetImage(name string) (*Image, error) {
+	if name == "" {
+		return nil, errors.New("image name is empty")
+	}
+
+	if strings.Count(name, "/") == 0 {
+		name = fmt.Sprintf("%s/%s", dockerDefaultRepoPrefix, name)
+	}
+
+	u, err := url.Parse(dockerIndexURL)
+	if err != nil {
+		return nil, err
+	}
+
+	// In order to get layers from Docker CDN we need to hit 'images' endpoint
+	// and request the token. Client should also accept and store cookies, as
+	// they are needed to fetch the layer data later.
+	imagesURL := fmt.Sprintf("%s/v1/repositories/%s/images", dockerIndexURL, name)
+
+	req, err := http.NewRequest("GET", imagesURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("X-Docker-Token", "true")
+
+	client := &http.Client{ // That's what Docker itself uses.
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+			Proxy:             http.ProxyFromEnvironment,
+		},
+	}
+	client.Jar, err = cookiejar.New(nil) // Docker repo API sets and uses cookies for CDN.
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP %d ", res.StatusCode)
+	}
+
+	token := res.Header.Get("X-Docker-Token")
+	endpoints := strings.Split(res.Header.Get("X-Docker-Endpoints"), ",")
+
+	if len(endpoints) == 0 {
+		return nil, errors.New("Docker index response didn't contain any endpoints")
+	}
+	for i := range endpoints {
+		endpoints[i] = strings.Trim(endpoints[i], " ")
+	}
+
+	img := &Image{
+		Name:      name,
+		client:    client,
+		endpoints: endpoints,
+		token:     token,
+		scheme:    u.Scheme,
+	}
+
+	img.tags, err = img.fetchTags()
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+// Metadata unmarshals a Docker image metadata into provided 'v' interface.
+func (i *Image) Metadata(tagName string, v interface{}) error {
+	layerID, ok := i.tags[tagName]
+	if !ok {
+		return fmt.Errorf("can't find tag '%s' for image '%s'", tagName, i.Name)
+	}
+
+	err := i.parseResponse(fmt.Sprintf("v1/images/%s/json", layerID), &v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// History returns an ordered list of layers that make up Docker. The order is reverse, it goes from
+// the latest layer to the base layer. Client can iterate these layers and download them using LayerReader.
+func (i *Image) History(tagName string) ([]string, error) {
+	layerID, ok := i.tags[tagName]
+	if !ok {
+		return nil, fmt.Errorf("can't find tag '%s' for image '%s'", tagName, i.Name)
+	}
+
+	var history []string
+	err := i.parseResponse(fmt.Sprintf("v1/images/%s/ancestry", layerID), &history)
+	if err != nil {
+		return nil, err
+	}
+	return history, nil
+}
+
+// LayerReader returns io.ReadCloser that can be used to read Docker layer data.
+func (i *Image) LayerReader(id string) (io.ReadCloser, error) {
+	resp, err := i.getResponse(fmt.Sprintf("v1/images/%s/layer", id))
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}
+
+// fetchTags fetches tags for the image and caches them in the Image struct,
+// so that other methods can look them up efficiently.
+func (i *Image) fetchTags() (map[string]string, error) {
+	// There is a weird quirk about Docker API: if tags are requested from index.docker.io,
+	// it returns a list of short layer IDs, so it's impossible to use them to download actual layers.
+	// However, when we hit the endpoint returned by image index API response, it has an expected format.
+	var tags map[string]string
+	err := i.parseResponse(fmt.Sprintf("v1/repositories/%s/tags", i.Name), &tags)
+	if err != nil {
+		return nil, err
+	}
+	return tags, nil
+}
+
+// getAPIResponse takes a path and tries to get Docker API response from each
+// available Docker API endpoint. It returns raw HTTP response.
+func (i *Image) getResponse(path string) (*http.Response, error) {
+	errors := make(map[string]error)
+
+	for _, ep := range i.endpoints {
+		resp, err := i.getResponseFromURL(fmt.Sprintf("%s://%s/%s", i.scheme, ep, path))
+		if err != nil {
+			errors[ep] = err
+			continue
+		}
+
+		return resp, nil
+	}
+
+	return nil, combineEndpointErrors(errors)
+}
+
+// parseJSONResponse takes a path and tries to get Docker API response from each
+// available Docker API endpoint. It tries to parse response as JSON and saves
+// the parsed version in the provided 'result' variable.
+func (i *Image) parseResponse(path string, result interface{}) error {
+	errors := make(map[string]error)
+
+	for _, ep := range i.endpoints {
+		err := i.parseResponseFromURL(fmt.Sprintf("%s://%s/%s", i.scheme, ep, path), result)
+		if err != nil {
+			errors[ep] = err
+			continue
+		}
+
+		return nil
+	}
+
+	return combineEndpointErrors(errors)
+}
+
+// getAPIResponseFromURL returns raw Docker API response at URL 'u'.
+func (i *Image) getResponseFromURL(u string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Token "+i.token)
+
+	res, err := i.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		res.Body.Close()
+		return nil, fmt.Errorf("%s: HTTP %d", u, res.StatusCode)
+	}
+
+	return res, nil
+}
+
+// parseResponseFromURL returns parsed JSON of a Docker API response at URL 'u'.
+func (i *Image) parseResponseFromURL(u string, result interface{}) error {
+	resp, err := i.getResponseFromURL(u)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&result); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// combineEndpointErrors takes a mapping of Docker API endpoints to errors encountered
+// while talking to them and returns a single error that contains all endpoint URLs
+// along with error for each URL.
+func combineEndpointErrors(allErrors map[string]error) error {
+	var parts []string
+	for ep, err := range allErrors {
+		parts = append(parts, fmt.Sprintf("%s: %s", ep, err))
+	}
+	return errors.New(strings.Join(parts, ", "))
+}
---
+++ docker/registry_test.go
@@ -0,0 +1,240 @@
+// Copyright 2014 Apcera Inc. All rights reserved.
+// Borrowing general test structure from Docker mock registry test.
+
+package docker
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/apcera/cntm-deps/mux"
+
+	tt "github.com/apcera/util/testtool"
+)
+
+var (
+	testVerbose    = false // Change to true in order to see HTTP requests in test output.
+	testHttpServer *httptest.Server
+	testLayers     = map[string]map[string]string{
+		"badcafe": {
+			"json":     `{"id":"badcafe","k1": "v1"}`,
+			"ancestry": `["badcafe"]`,
+			"layer":    string([]byte{0xa1, 0xb2, 0xc3}),
+			"checksum": "1ac330d",
+		},
+		"deadbeef": {
+			"json":     `{"id":"deadbeef","k2": "v2"}`,
+			"ancestry": `["deadbeef", "badcafe"]`,
+			"layer":    string([]byte{0xd4, 0xe5, 0xf6}),
+			"checksum": "2bd330f",
+		},
+	}
+	testRepositories = map[string]map[string]string{
+		"foo/bar": {
+			"latest": "deadbeef",
+			"base":   "badcafe",
+		},
+		"library/base": {
+			"latest": "badcafe",
+		},
+	}
+)
+
+func init() {
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/images/{image_id:[^/]+}/{data_type:json|layer|ancestry}", handlerImage).Methods("GET")
+	r.HandleFunc("/v1/repositories/{repository:.+}/tags", handlerTags).Methods("GET")
+	r.HandleFunc("/v1/repositories/{repository:.+}/images", handlerImages).Methods("GET")
+
+	testHttpServer = httptest.NewServer(logHandler(r))
+	dockerIndexURL = testHttpServer.URL
+}
+
+func logHandler(handler http.Handler) http.Handler {
+	if !testVerbose {
+		return handler
+	}
+	lh := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s \"%s %s\"\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(lh)
+}
+
+func writeResponse(w http.ResponseWriter, httpStatus int, payload interface{}) {
+	w.WriteHeader(httpStatus)
+	body, err := json.Marshal(payload)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	w.Write(body)
+}
+
+func checkAuth(w http.ResponseWriter, r *http.Request) bool {
+	writeAuth := func() {
+		sessionID := fmt.Sprintf("FAKE-SESSION-%d", time.Now().UnixNano())
+		token := fmt.Sprintf("FAKE-TOKEN-%d", time.Now().UnixNano())
+
+		http.SetCookie(w, &http.Cookie{Name: "session", Value: sessionID, MaxAge: 3600})
+		w.Header().Add("X-Docker-Token", token)
+	}
+
+	// Mock registry doesn't really perform any auth.
+	if len(r.Cookies()) > 0 {
+		writeAuth()
+		return true
+	}
+
+	if (len(r.Header.Get("Authorization"))) > 0 {
+		writeAuth()
+		return true
+	}
+	w.Header().Add("WWW-Authenticate", "token")
+	writeResponse(w, 401, "Bad auth")
+	return false
+}
+
+func handlerImages(w http.ResponseWriter, r *http.Request) {
+	u, _ := url.Parse(testHttpServer.URL) // Ignoring error, URL is guaranteed to be OK.
+
+	w.Header().Add("X-Docker-Endpoints", fmt.Sprintf("%s     ,    %s", u.Host, "test.example.com"))
+	w.Header().Add("X-Docker-Token", fmt.Sprintf("FAKE-SESSION-%d", time.Now().UnixNano()))
+
+	var images []map[string]string
+
+	for imageID, layer := range testLayers {
+		image := make(map[string]string)
+		image["id"] = imageID
+		image["checksum"] = layer["checksum"]
+		images = append(images, image)
+	}
+	writeResponse(w, 200, images)
+}
+
+func handlerImage(w http.ResponseWriter, r *http.Request) {
+	if !checkAuth(w, r) {
+		return
+	}
+
+	vars := mux.Vars(r)
+	layer, exists := testLayers[vars["image_id"]]
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	layer_size := len(layer["layer"])
+	w.Header().Add("X-Docker-Size", strconv.Itoa(layer_size))
+	io.WriteString(w, layer[vars["data_type"]])
+}
+
+func handlerTags(w http.ResponseWriter, r *http.Request) {
+	if !checkAuth(w, r) {
+		return
+	}
+
+	vars := mux.Vars(r)
+	tags, exists := testRepositories[vars["repository"]]
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	writeResponse(w, 200, tags)
+}
+
+func TestGetImage(t *testing.T) {
+	tt.StartTest(t)
+	defer tt.FinishTest(t)
+
+	img, err := GetImage("")
+	tt.TestExpectError(t, err)
+	tt.TestEqual(t, err.Error(), "image name is empty")
+
+	img, err = GetImage("foo/bar")
+	tt.TestExpectSuccess(t, err)
+
+	tt.TestEqual(t, img.Name, "foo/bar")
+
+	img, err = GetImage("base")
+	tt.TestExpectSuccess(t, err)
+
+	tt.TestEqual(t, img.Name, "library/base")
+}
+
+func TestGetImageHistory(t *testing.T) {
+	tt.StartTest(t)
+	defer tt.FinishTest(t)
+
+	img, err := GetImage("foo/bar")
+	tt.TestExpectSuccess(t, err)
+
+	h, err := img.History("tag2")
+	tt.TestExpectError(t, err)
+	tt.TestEqual(t, err.Error(), "can't find tag 'tag2' for image 'foo/bar'")
+
+	h, err = img.History("latest")
+	tt.TestExpectSuccess(t, err)
+	tt.TestEqual(t, len(h), 2)
+	tt.TestEqual(t, h[0], "deadbeef")
+	tt.TestEqual(t, h[1], "badcafe")
+}
+
+func TestGetImageMetadata(t *testing.T) {
+	tt.StartTest(t)
+	defer tt.FinishTest(t)
+
+	img, err := GetImage("foo/bar")
+	tt.TestExpectSuccess(t, err)
+
+	var m1 map[string]interface{}
+	err = img.Metadata("tag2", &m1)
+	tt.TestExpectError(t, err)
+	tt.TestEqual(t, err.Error(), "can't find tag 'tag2' for image 'foo/bar'")
+
+	var m2 map[string]interface{}
+	err = img.Metadata("latest", &m2)
+	tt.TestExpectSuccess(t, err)
+	tt.TestEqual(t, len(m2), 2)
+	tt.TestEqual(t, m2["id"], "deadbeef")
+	tt.TestEqual(t, m2["k2"], "v2")
+
+	var m3 map[string]interface{}
+	err = img.Metadata("base", &m3)
+	tt.TestExpectSuccess(t, err)
+	tt.TestEqual(t, len(m3), 2)
+	tt.TestEqual(t, m3["id"], "badcafe")
+	tt.TestEqual(t, m3["k1"], "v1")
+}
+
+func TestReadLayer(t *testing.T) {
+	tt.StartTest(t)
+	defer tt.FinishTest(t)
+
+	img, err := GetImage("foo/bar")
+	tt.TestExpectSuccess(t, err)
+
+	r, err := img.LayerReader("deadbeef")
+	tt.TestExpectSuccess(t, err)
+	body, err := ioutil.ReadAll(r)
+	tt.TestExpectSuccess(t, err)
+	tt.TestEqual(t, body, []byte{0xd4, 0xe5, 0xf6})
+
+	r, err = img.LayerReader("badcafe")
+	tt.TestExpectSuccess(t, err)
+	body, err = ioutil.ReadAll(r)
+	tt.TestExpectSuccess(t, err)
+	tt.TestEqual(t, body, []byte{0xa1, 0xb2, 0xc3})
+
+	r, err = img.LayerReader("badbad")
+	tt.TestExpectError(t, err)
+}
