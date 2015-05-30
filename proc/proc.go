d798bcc3280664d2a90b6a64fe8dcb953a49921a
Shammah Chancellor <shammah@apcera.com>
2015-05-30 01:26:20 -0700

    proc: MountPoints by device.

--- proc/proc.go
+++ proc/proc.go
@@ -75,6 +75,62 @@ func MountPoints() (map[string]*MountPoint, error) {
 	return mp, nil
 }
 
+// Reads through /proc/mounts and returns the data associated devices
+func MountPointsByDevice() (map[string][]*MountPoint, error) {
+	mp := make(map[string][]*MountPoint)
+	var current *MountPoint
+	err := ParseSimpleProcFile(
+		MountProcFile,
+		nil,
+		func(line int, index int, elm string) error {
+			switch index {
+			case 0:
+				current = new(MountPoint)
+				current.Dev = elm
+				if mpSlice, ok := mp[current.Dev]; ok {
+					mpSlice = append(mpSlice, current)
+				} else {
+					mp[current.Dev] = []*MountPoint{current}
+				}
+			case 1:
+				if len(elm) > 0 && elm[0] != '/' {
+					return fmt.Errorf("Invalid path on line %d of file %s: %s",
+						line, MountProcFile, elm)
+				}
+				current.Path = elm
+			case 2:
+				current.Fstype = elm
+			case 3:
+				current.Options = elm
+			case 4:
+				n, err := strconv.ParseUint(elm, 10, 32)
+				if err != nil {
+					return fmt.Errorf(
+						"Error parsing column %d on line %d of file %s: %s",
+						index, line, MountProcFile, elm)
+				}
+				current.Dump = int(n)
+			case 5:
+				n, err := strconv.ParseUint(elm, 10, 32)
+				if err != nil {
+					return fmt.Errorf(
+						"Error parsing column %d on line %d of file %s: %s",
+						index, line, MountProcFile, elm)
+				}
+				current.Fsck = int(n)
+			default:
+				return fmt.Errorf(
+					"Too many colums on line %d of file %s",
+					line, MountProcFile)
+			}
+			return nil
+		})
+	if err != nil {
+		return nil, err
+	}
+	return mp, nil
+}
+
 // Stores interface statistics that are gleaned from /proc/dev/net.
 type InterfaceStat struct {
 	Device       string
