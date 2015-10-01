15c7dd84b3e3796aacfbba3515493be8d4372d8f
Jon Poler <jonathan.poler@apcera.com>
2015-10-01 10:10:24 -0700

    Add a package to render TaskEvents from the Task API Framework.
    This renderer acts as a simple append-only logger. The main idea
    is that the renderer formats incoming TaskEvents in a human-readable
    way. TaskEvents will be provided to the renderer via a channel, and
    the renderer will output them to the specified io.Writer.

---
+++ taskrenderer/renderer.go
@@ -0,0 +1,73 @@
+// Copyright 2015 Apcera Inc. All rights reserved.
+
+package taskrenderer
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+// Renderer holds all of the state necessary to gather and output TaskEvents.
+type Renderer struct {
+	out io.Writer
+	err io.Writer
+
+	eventCh chan *TaskEvent
+
+	options *FormatOptions
+}
+
+// FormatOptions provides a way to customize the output of TaskEvents.
+type FormatOptions struct {
+	showTime bool
+}
+
+// New instantiates and returns a new Renderer object.
+func New(out io.Writer, err io.Writer, showTime bool) *Renderer {
+	return &Renderer{
+		out: out,
+		err: err,
+		options: &FormatOptions{
+			showTime: showTime,
+		},
+	}
+}
+
+// RenderEvents reads every event sent on the given channel and renders it.
+// The channel can be closed by the caller at any time to stop rendering.
+func (r *Renderer) RenderEvents(eventCh chan *TaskEvent) {
+	for event := range eventCh {
+		r.renderEvent(event)
+	}
+}
+
+// renderEvent varies output depending on the information provided
+// by the current taskEvent.
+func (r *Renderer) renderEvent(event *TaskEvent) {
+	s := ""
+
+	if r.options.showTime {
+		s += fmt.Sprintf("(%s) ", time.Unix(0, event.Time).Format(time.UnixDate))
+	}
+
+	if event.Thread != "" {
+		s += fmt.Sprintf("[%s] -- ", event.Thread)
+	}
+
+	s += fmt.Sprintf("%s -- ", event.Stage)
+
+	if event.Subtask.Total != 1 {
+		s += fmt.Sprintf("(%d/%d): ", event.Subtask.Index, event.Subtask.Total)
+	}
+
+	s += fmt.Sprintf("%s", event.Subtask.Name)
+
+	if event.Subtask.Progress.Total != 0 {
+		s += fmt.Sprintf(" ... %d%%",
+			(event.Subtask.Progress.Current/event.Subtask.Progress.Total)*100,
+		)
+	}
+
+	fmt.Fprintf(r.out, "%s\n", s)
+}
