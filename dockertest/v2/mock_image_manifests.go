0f53929c1d435ede869802ae9687f19283676142
Alex Toombs <alex.m.toombs@gmail.com>
2015-11-30 11:32:48 -0800

    dockertest: add a simple mock v2 registry
    No signature verification support; dummy content given for blobs.

---
+++ dockertest/v2/mock_image_manifests.go
@@ -0,0 +1,9 @@
+// Copyright 2015 Apcera Inc. All rights reserved.
+
+package v2
+
+const (
+	// libraryNatsLatestManifest is the manifest for the library/nats image.
+	// Omits history for brevity.
+	libraryNatsLatestManifest = `{"schemaVersion":1,"name":"library/nats","tag":"latest","architecture":"amd64","fsLayers":[{"blobSum":"sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"},{"blobSum":"sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"},{"blobSum":"sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"},{"blobSum":"sha256:3f9c4e15d7dcbe265a38cca404cb0525dbf1124b34b15bed9f4df5cdfbf82370"},{"blobSum":"sha256:8b754456ca2582d79d592eea1d0e1df8704bce2dff4f0931cf00007fed78275c"},{"blobSum":"sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"}]}`
+)
