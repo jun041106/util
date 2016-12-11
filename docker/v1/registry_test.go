62fddb71aad4417bc9c02703964605c6d176de73
John Schnake <john@apcera.com>
2016-12-11 00:49:30 -0600

    testtool: Use DeepEqual
     - Previously we were skipping over unexported fields silently which
    gives misleading results in many situations (e.g. two different
    byte buffers with different contents would pass TestEqual). Included
    many changes to how deepValueEquals iterates over values.
    
     - Added one more helper (TestNotMatch).
    
     - Updated method signatures to match expectations in our other codebase
    including creating a TestTool object rather than relying only on global
    state.
    
     - Misc readability fixes

--- docker/v1/registry_test.go
+++ docker/v1/registry_test.go
@@ -19,8 +19,8 @@ func init() {
 }
 
 func TestGetImage(t *testing.T) {
-	tt.StartTest(t)
-	defer tt.FinishTest(t)
+	testHelper := tt.StartTest(t)
+	defer testHelper.FinishTest()
 
 	img, statusCode, err := GetImage("", "")
 	tt.TestExpectError(t, err)
@@ -39,8 +39,8 @@ func TestGetImage(t *testing.T) {
 }
 
 func TestGetImageHistory(t *testing.T) {
-	tt.StartTest(t)
-	defer tt.FinishTest(t)
+	testHelper := tt.StartTest(t)
+	defer testHelper.FinishTest()
 
 	img, statusCode, err := GetImage("foo/bar", "")
 	tt.TestExpectSuccess(t, err)
@@ -58,8 +58,8 @@ func TestGetImageHistory(t *testing.T) {
 }
 
 func TestGetImageTags(t *testing.T) {
-	tt.StartTest(t)
-	defer tt.FinishTest(t)
+	testHelper := tt.StartTest(t)
+	defer testHelper.FinishTest()
 
 	img, statusCode, err := GetImage("foo/bar", "")
 	tt.TestExpectSuccess(t, err)
@@ -71,8 +71,8 @@ func TestGetImageTags(t *testing.T) {
 }
 
 func TestGetImageTagLayerID(t *testing.T) {
-	tt.StartTest(t)
-	defer tt.FinishTest(t)
+	testHelper := tt.StartTest(t)
+	defer testHelper.FinishTest()
 
 	img, statusCode, err := GetImage("foo/bar", "")
 	tt.TestExpectSuccess(t, err)
@@ -92,8 +92,8 @@ func TestGetImageTagLayerID(t *testing.T) {
 }
 
 func TestGetImageMetadata(t *testing.T) {
-	tt.StartTest(t)
-	defer tt.FinishTest(t)
+	testHelper := tt.StartTest(t)
+	defer testHelper.FinishTest()
 
 	img, statusCode, err := GetImage("foo/bar", "")
 	tt.TestExpectSuccess(t, err)
@@ -120,8 +120,8 @@ func TestGetImageMetadata(t *testing.T) {
 }
 
 func TestReadLayer(t *testing.T) {
-	tt.StartTest(t)
-	defer tt.FinishTest(t)
+	testHelper := tt.StartTest(t)
+	defer testHelper.FinishTest()
 
 	img, statusCode, err := GetImage("foo/bar", "")
 	tt.TestExpectSuccess(t, err)
